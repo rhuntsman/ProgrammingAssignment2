398a592a4ce6d085821aa5d39d229d263fd7f09f
rhuntsman <robert.huntsman@gmail.com>
2015-02-16 16:28:30 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function returns a special matrix from a matrix with 4 functions that:
+## (1) Set the matrix (2) get the original matrix (3) set the inverse of the matrix and (4) return
+## the inverse of the matrix. Used in conjunction with cacheSolve, the two functions return
+## the inverse of a matrix, either from memory (if already calculated) or using the Solve function
 
-## Write a short comment describing this function
+## This code creates a special matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL #set inv to null first time
@@ -18,19 +20,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function either returns the inverse of the special matrix in memory if already calculated or it calculates
+## the inverse of the special matrix object using Solve if not already calculated
 
 cacheSolve <- function(x, ...) {
         
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
+  if(!is.null(inv)) { ## check to see if the inverse matrix already exists
+    message("getting cached data") 
+    return(inv)  ## return the inverse matrix in memory
   }
-  original <- x$get()
-  inv <- solve(original) # return the inverse of the original matrix
-  x$setinv(inv)
-  inv
+  original <- x$get() ## if inverse is not in memory, get the original matrix
+  inv <- solve(original) # return the inverse of the original matrix ## calculate the inverse of the matrix
+  x$setinv(inv) ## set the inverse of the matrix in memory
+  inv ## return the matrix
 }
 
